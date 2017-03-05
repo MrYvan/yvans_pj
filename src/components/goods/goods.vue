78e2d3a1ffe271baa6191727326d6fec8c3ec44a
Yvan <346293882@qq.com>
2017-03-05 11:48:26 +0800

    menu及foods开发完成

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,18 +1,45 @@
 <template lang="html">
     <div class="goods">
-      <div class="menu-wrapper">
+      <div class="menu-wrapper" v-el:menu-wrapper>
         <ul>
-          <li v-for="item in goods" class="menu-item">
+          <li v-for="item in goods" class="menu-item" :class="{'current':
+          currentIndex===$index}" @click="selectMenu($index,$event)">
             <span class="text border-1px">
               <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
             </span>
           </li>
         </ul>
       </div>
-      <div class="foods-wrapper"></div>
+      <div class="foods-wrapper" v-el:foods-wrapper>
+        <ul>
+          <li v-for="item in goods" class="foods-list food-list-hook">
+            <h1 class="title">{{item.name}}</h1>
+            <ul>
+              <li v-for="food in item.foods" class="food-item">
+                <div class="icon">
+                  <img width="57" height="57" :src="food.icon">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.desciption}}</p>
+                  <div class="extra">
+                    <span>月售{{food.sellCount}}份</span>
+                    <span>好评率{{food.rating}}%</span>
+                  </div>
+                  <div class="price">
+                    <span class="now">￥{{food.price}}</span>
+                    <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
 </template>
 <script type="text/ecmascript-6">
+    import BScroll from 'better-scroll'
     const ERR_OK = 0
     export default {
       props: {
@@ -22,7 +49,21 @@
       },
       data() {
         return {
-          goods:[]
+          goods:[],
+          listHeight:[],
+          scrollY:0
+        }
+      },
+      computed:{
+        currentIndex() {
+          for(let i=0;i<this.listHeight.length;i++) {
+            let height1 = this.listHeight[i]
+            let height2 = this.listHeight[i+1]
+            if(!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+              return i
+            }
+          }
+          return 0
         }
       },
       created() {
@@ -31,8 +72,44 @@
           response = response.body
           if( response.errno === ERR_OK ) {
             this.goods = response.data
+            this.$nextTick(() => {
+                this._initScroll()
+                this._calculateHeight()
+              }
+            )
           }
         })
+      },
+      methods: {
+        selectMenu(index, event) {
+          if(!event._constructed) {
+            return
+          }
+          let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook')
+          let el = foodList[index]
+          this.foodsScroll.scrollToElement(el, 300)
+        },
+        _initScroll() {
+          this.menuScroll = new BScroll(this.$els.menuWrapper, {
+            click:true
+          })
+          this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
+            probeType: 3
+          })
+          this.foodsScroll.on('scroll', (pos) => {
+            this.scrollY = Math.abs(Math.round(pos.y))
+          })
+        },
+        _calculateHeight() {
+          let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook')
+          let height = 0
+          this.listHeight.push(height)
+          for(let i = 0; i<foodList.length; i++) {
+            let item = foodList[i]
+            height += item.clientHeight
+            this.listHeight.push(height)
+          }
+        }
       },
       components:{}
     }
@@ -56,6 +133,14 @@
         height:54px
         line-height:14px
         padding: 0 12px
+        &.current
+          position:relative
+          z-index:10
+          margin-top:-1px
+          font-weight:700
+          background:#fff
+          .text
+            border-none()
         .icon
           display:inline-block
           vertical-align:top
@@ -82,5 +167,52 @@
           font-size:12px
     .foods-wrapper
       flex:1
+      .title
+        padding-left:14px
+        height:26px
+        line-height:26px
+        border-left:2px solid #d9dde1
+        font-size:12px
+        color:rgb(147,153,159)
+        background:#f3f5f7
+      .food-item
+        display:flex
+        margin:18px
+        padding-bottom:18px
+        border-1px(rgba(7,17,27,0.1))
+        &:last-child
+          margin-bottom:0
+          border-none()
+        .icon
+          flex:0 0 57px
+          margin-right:10px
+        .content
+          flex:1
+          .name
+            margin:2px 0 8px 0
+            height:14px
+            line-height:14px
+            color:rgb(7,17,27)
+           .desc,.extra
+            font-size:10px
+            line-height:10px
+            color:rgb(147,153,159)
+           .desc
+              margin-bottom:8px
+              line-height:12px
+           .extra
+              &.count
+                margin-right:12px
+            .price
+              font-weight:700
+              line-height:24px
+              .now
+                margin-right:8px
+                fon-size:14px
+                color:rgb(240,20,20)
+              .old
+                text-decoration:line-through
+                font-size:10px
+                color:rgb(147,153,159)
 
 </style>
